20c9a0f317aca045850d4e34f919802761610df5
Steven Haryanto <stevenharyanto@gmail.com>
2013-11-13 18:05:53 +0000

    Import of SHARYANTO/Perinci-Examples-Bin-0.02 from CPAN.
    gitpan-cpan-distribution: Perinci-Examples-Bin
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SHARYANTO/Perinci-Examples-Bin-0.02.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Perinci/Examples/Bin.pm
+++ lib/Perinci/Examples/Bin.pm
@@ -1,13 +1,15 @@
 package Perinci::Examples::Bin;
 
-our $VERSION = '0.01'; # VERSION
+our $VERSION = '0.02'; # VERSION
 
 1;
 # ABSTRACT: Perinci example scripts
 
-
 __END__
+
 =pod
+
+=encoding UTF-8
 
 =head1 NAME
 
@@ -15,7 +17,7 @@ Perinci::Examples::Bin - Perinci example scripts
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 DESCRIPTION
 
@@ -23,15 +25,21 @@ This distribution contains example scripts in bin/. It is split from
 L<Perinci::Example> so the latter does not need to depend on
 L<Perinci::CmdLine>.
 
-=head1 DESCRIPTION
+=head1 HOMEPAGE
 
+Please visit the project's homepage at L<https://metacpan.org/release/Perinci-Examples-Bin>.
 
-This module has L<Rinci> metadata.
+=head1 SOURCE
 
-=head1 FUNCTIONS
+Source repository is at L<https://github.com/sharyanto/perl-Perinci-Examples-Bin>.
 
+=head1 BUGS
 
-None are exported by default, but they are exportable.
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Perinci-Examples-Bin>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
 
 =head1 AUTHOR
 
@@ -39,10 +47,9 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by Steven Haryanto.
+This software is copyright (c) 2013 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
